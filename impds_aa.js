5f61923e15e7d92230fc0e29a1c2518760318c4e
parasjain286 <141493394+parasjain286@users.noreply.github.com>
2026-04-17 18:49:30 +0530

    Update impds_aa.js

--- impds_aa.js
+++ impds_aa.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const CryptoJS = require('crypto-js');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const ENCRYPTION_KEY = "nic@impds#dedup05613";
 
