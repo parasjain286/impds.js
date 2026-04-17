2a1cf81f32e3031e986208d8ae797d196cdf4e26
parasjain286 <141493394+parasjain286@users.noreply.github.com>
2026-04-17 22:16:22 +0530

    Update impds_auth.py

--- impds_auth.py
+++ impds_auth.py
@@ -6,7 +6,16 @@ import re
 import pytesseract  
 from PIL import Image  
 import io  
-  
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Service running"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 class IMPDSAutomation:  
     def __init__(self):  
         self.session = requests.Session()  
